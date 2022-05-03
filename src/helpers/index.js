492022d4abe66de2237f6510e590e666d3fe337d
Szymon Rojek <sz.rojek@gmail.com>
2022-05-03 15:12:04 +0100

    edit exports

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,6 +1,5 @@
 export { default as getDataFromHtmlTable } from "./getDataFromHtmlTable";
 export { default as addFormattedTimeToData } from "./addFormattedTimeToData";
-export { default as loadData } from "./LoadData";
 export { default as toggleArrow } from "./toggleArrow";
-export { default as sortDataTime } from "./sortDataTime";
-export { default as sortStringData } from "./sortStringData";
+export { default as getAscendingOrderData } from "./getAscendingOrderData";
+export { default as getDescendingOrderData } from "./getDescendingOrderData";
